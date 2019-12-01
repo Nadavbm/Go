47b64d28fed353f91ff2bd0cf1c85197935ed7f3
Nadav <nadavbm@outlook.com>
2019-12-01 17:29:52 +0100

    Simple reader functions

---
+++ file/reader/README.txt
@@ -0,0 +1,8 @@
+This is an empty file
+Read function can read the lines
+from here.
+
+Functions:
+Read and print lines
+Read and send via os function
+Read line by line
---
+++ file/reader/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"log"
+	//"io"
+	"io/ioutil"
+	
+)
+
+var path = "./README.txt"
+
+func main() {
+	fmt.Println("The log file located at: ", PrintPath())
+	fmt.Println("===========================================")
+	fmt.Println("The file content in bytes slice:")
+	fmt.Println("================================")
+	ReadAsBuffer()
+	fmt.Println("\nAnd now convert bytes to text:")
+	fmt.Println("==============================")
+	ReadAsText()
+}
+
+func ReadAsText() {
+	text, err := ioutil.ReadFile("README.txt")
+	if err != nil {
+		log.Fatal(err)
+		fmt.Println("Cannot open file", err)
+	}
+	fmt.Println("", string(text))
+}
+
+func ReadAsBuffer() {
+	file, err := os.OpenFile(path, os.O_RDWR, 0644)
+	// Retrun error is cannot open the file
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Close file at the end of this function
+	defer file.Close()
+	
+	
+	text, err := ioutil.ReadAll(file)
+	fmt.Print(text)
+}
+
+func PrintPath() string {
+	return path
+}
+
+
