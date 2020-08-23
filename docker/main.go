688c63c89dba6278759b51a746931a1513d7d85b
Nadav <nadavbm@outlook.com>
2020-08-23 21:25:50 +0200

    managing containers with docker api go client

--- docker/main.go
+++ docker/main.go
@@ -3,24 +3,57 @@ package main
 import (
 	"context"
 	"fmt"
+	"io"
 	"log"
+	"os"
 
 	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/client"
 )
 
 func main() {
-	err := listContainers()
+	err := ListContainers()
 	if err != nil {
-		log.Println("ERROR:", err)
+		log.Println("ERROR: listing docker container:", err)
+	}
+
+	err = ListVolumes()
+	if err != nil {
+		log.Println("ERROR: listing docker voluems:", err)
+	}
+
+	err = CreateContainer()
+	if err != nil {
+		log.Println("ERROR: creating container:", err)
+	}
+
+	err = StopAllRunningContainers()
+	if err != nil {
+		log.Println("ERROR: creating container:", err)
 	}
 }
 
-func listContainers() error {
-	config, err := setConfig()
+func SetContainerConfig() error {
+	//config, err := client.ContainerAPIClient()
+	return nil
+}
+
+func SetConfig() (*client.Client, error) {
+	config, err := client.NewEnvClient()
+	if err != nil {
+		return nil, err
+	}
+	return config, nil
+}
+
+// list all docker containers - docker ps
+func ListContainers() error {
+	config, err := SetConfig()
 	if err != nil {
 		return err
 	}
+
 	containers, err := config.ContainerList(context.Background(), types.ContainerListOptions{})
 	if err != nil {
 		return err
@@ -34,10 +67,54 @@ func listContainers() error {
 	return nil
 }
 
-func setConfig() (*client.Client, error) {
-	config, err := client.NewClientWithOpts(client.FromEnv)
+// create a docker container
+func CreateContainer() error {
+	cli, err := SetConfig()
 	if err != nil {
-		return nil, err
+		log.Println(err, "")
 	}
-	return config, nil
+
+	reader, err := cli.ImagePull(context.Background(), "docker.io/library/alpine", types.ImagePullOptions{})
+	if err != nil {
+		log.Println(err, "")
+	}
+	io.Copy(os.Stdout, reader)
+
+	resp, err := cli.ContainerCreate(context.Background(), &container.Config{
+		Image: "alpine",
+		Cmd:   []string{"echo", "hello world"},
+		Tty:   false,
+	}, nil, nil, "")
+	if err != nil {
+		panic(err)
+	}
+
+	if err := cli.ContainerStart(context.Background(), resp.ID, types.ContainerStartOptions{}); err != nil {
+		log.Println(err, "start container error")
+	}
+
+	fmt.Println(resp.ID)
+	return nil
+}
+
+func StopAllRunningContainers() error {
+	ctx := context.Background()
+	cli, err := SetConfig()
+	if err != nil {
+		log.Println(err, "set config error")
+	}
+
+	containers, err := cli.ContainerList(ctx, types.ContainerListOptions{})
+	if err != nil {
+		log.Println(err, "listing container error")
+	}
+
+	for _, container := range containers {
+		fmt.Print("Stopping container ", container.ID[:10], "... ")
+		if err := cli.ContainerStop(ctx, container.ID, nil); err != nil {
+			log.Println(err, "stopping container error")
+		}
+		fmt.Println("Success")
+	}
+	return nil
 }
