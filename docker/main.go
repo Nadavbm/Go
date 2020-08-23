79cc01ff8498ddc325047a300e8b1fc8b7fac501
nadavbm <nadavbm@outlook.com>
2020-08-23 16:37:02 +0200

    go client for docker

--- docker/main.go
+++ docker/main.go
@@ -1,11 +1,43 @@
 package main
 
 import (
-	dock "github.com/nadavbm/go/docker/pkg/docker"
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/client"
 )
 
 func main() {
-	var operator dock.DockerOperator
+	err := listContainers()
+	if err != nil {
+		log.Println("ERROR:", err)
+	}
+}
 
-	operator.ListContainers()
+func listContainers() error {
+	config, err := setConfig()
+	if err != nil {
+		return err
+	}
+	containers, err := config.ContainerList(context.Background(), types.ContainerListOptions{})
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("docker containers:\nid\t\tname\t\tstatus")
+	for _, container := range containers {
+		fmt.Printf("%s\t%s\t%s\n", container.ID[:10], container.Image, container.Status)
+	}
+
+	return nil
+}
+
+func setConfig() (*client.Client, error) {
+	config, err := client.NewClientWithOpts(client.FromEnv)
+	if err != nil {
+		return nil, err
+	}
+	return config, nil
 }
